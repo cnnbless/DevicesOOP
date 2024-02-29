da67230e5e624ef0a508dbd1d43a23e4b6eaac71
cnnbless <antongolubchuk@gmail.com>
2024-02-29 13:59:38 +0200

    Змінили main.cpp

--- main.cpp
+++ main.cpp
@@ -2,14 +2,35 @@
 #include "Mouse.h"
 #include "Keyboard.h"
 #include "Headphones.h"
+
 using namespace std;
 int main() {
-    Keyboard warmila("Warmila",2,"plactic");
-    Mouse logitech("Logitech",0.3,"aluminium",true);
-    Headphones hyperx("HyperX",10,"metal",true);
+    //Keyboard warmila("Warmila",2,"plactic");
+    //Mouse logitech("Logitech",0.3,"aluminium",true);
+    //Headphones hyperx("HyperX",10,"metal",true);
+    //warmila.info();
+    //logitech.info();
+    //hyperx.info();
+    //
+   /* const Keyboard Warmila("warmila", 123, "plastic");
+    Warmila.get_TypeOfMaterial();*/
+/*   Mouse Hyperx("222", 222, "222", true);
+   Mouse vadim("212", 222, "222", true);
+   Logitech.get_AmountOfMice();*/
+/*    Mouse Logitech("123", 123, "123", true);
+    cin>>Logitech;
+    cout << Logitech;*/
+/*
+Mouse a("111", 0, "plastic");
+a+4;
+cout << a;
+a+=29.2;
+cout << a;
+a-3;
+cout << a;
+a-=12.2;
+cout << a;
+*/
 
-    warmila.info();
-    logitech.info();
-    hyperx.info();
     return 0;
 }
